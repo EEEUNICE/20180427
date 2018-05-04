fa1299e4cc9558ea63a61e98e08278b2530b3046
jane <229223133@qq.com>
2018-05-04 15:51:54 +0800

    dhakdha

--- routers/main.js
+++ routers/main.js
@@ -3,21 +3,21 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('main/mainpage');
+    res.render('main/index');
 })
 router.get('/application', function(req, res, next) {
-    res.render('issue1/application');
+    res.render('main/application');
 })
 router.get('/reports', function(req, res, next) {
-    res.render('issue1/reports');
+    res.render('main/reports');
 })
 router.get('/testimony', function(req, res, next) {
-    res.render('issue1/testimony');
+    res.render('main/testimony');
 })
 router.get('/video', function(req, res, next) {
-    res.render('issue1/video');
+    res.render('main/video');
 })
 router.get('/materials', function(req, res, next) {
-    res.render('issue1//materials');
+    res.render('main//materials');
 })
 module.exports = router;
